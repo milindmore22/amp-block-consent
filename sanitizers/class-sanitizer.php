1cf6d1ac73b6d794cf690c52c528d8172387b785
milindmore22 <milindmore22@gmail.com>
2021-12-08 23:43:59 +0530

    second Commit

--- sanitizers/class-sanitizer.php
+++ sanitizers/class-sanitizer.php
@@ -23,13 +23,26 @@ class Sanitizer extends AMP_Base_Sanitizer {
 	public function sanitize() {
 		$xpath = new DOMXPath( $this->dom );
 
-		// Set up mobile nav menu.
+		// Get all Instagram Embeds.
 		$amp_instagrams = $xpath->query( '//amp-instagram' );
+		$amp_youtubes = $xpath->query( '//amp-youtube' );
 
+		// Insta.
 		if ( $amp_instagrams instanceof DOMNodeList ) {
+			// Loop thourgh all instgarm embeds on page.
 			foreach ( $amp_instagrams as $amp_instagram ) {
 				if ( $amp_instagram instanceof DOMElement ) {
+					// Add Attribute.
 					$amp_instagram->setAttribute( 'data-block-on-consent', '' );
+				}
+			}
+		}
+
+		// Youtube.
+		if ( $amp_youtubes instanceof DOMNodeList ) {
+			foreach ( $amp_youtubes as $amp_youtube ) {
+				if ( $amp_youtube instanceof DOMElement ) {
+					$amp_youtube->setAttribute( 'data-block-on-consent', '' );
 				}
 			}
 		}
